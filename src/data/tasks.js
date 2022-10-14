eebf36216eaaba0922db70e0ad49c73f87d4863d
PietroArGal <pietromauricioag@outlook.com>
2022-10-14 18:39:01 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi Primera Tarea',
+        description: 'Mi Primera Desc'
+    },
+    {
+        id: 1,
+        title: 'Mi Segunda Tarea',
+        description: 'Mi Segunda Desc'
+    },
+    {
+        id: 2,
+        title: 'Mi Tercera Tarea',
+        description: 'Mi Tercera Desc'
+    },
+];
